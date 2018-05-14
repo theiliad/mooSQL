46a86ad5a3a12bcd8d159230cc64c4e1e82138ce
The Iliad <iliadmoosavi@gmail.com>
2018-05-14 01:03:39 -0400

    Notifications working

--- _components/App.js
+++ _components/App.js
@@ -20,11 +20,14 @@ import axios from "axios";
 // Antd
 import { Layout } from "antd";
 const { Header, Content, Sider } = Layout;
-import { Menu, Dropdown, Icon } from "antd";
+import { Menu, Dropdown, Icon } from "antd"
 const SubMenu = Menu.SubMenu;
-import { Form, Input } from "antd";
+import { Form, Input } from "antd"
 import { Badge } from "antd"
 import { Modal, Button } from 'antd'
+import { Tabs } from 'antd'
+const TabPane = Tabs.TabPane
+import { List, Avatar } from 'antd'
 
 // Components
 import HomeComponent from "./Home"
@@ -50,6 +53,40 @@ WebFont.load({
   }
 })
 
+import AVATARS_MARK from '../img/demo_assets/faces/mark.jpg'
+import AVATARS_SARAH from '../img/demo_assets/faces/sarah.jpg'
+import AVATARS_TOM from '../img/demo_assets/faces/tom.jpg'
+const notificationsData = [
+  {
+    title: 'Mark is asking for permissions',
+    desc: "Mark needs to access user records from Sept. 2016",
+    avatar: AVATARS_MARK
+  },
+  {
+    title: 'Sarah wants to meet',
+    desc: 'Sarah needs to "go over our design options" with you',
+    avatar: AVATARS_SARAH
+  },
+  {
+    title: 'You missed a call from Tom',
+    desc: "Tom called you on Tuesday May 6th. at 9:30am",
+    avatar: AVATARS_TOM
+  }
+];
+
+const messageData = [
+  {
+    title: 'Sarah Vandesky',
+    desc: "The soundwaves illustration is looking really great",
+    avatar: AVATARS_SARAH
+  },
+  {
+    title: 'Tom White',
+    desc: "We should get back to them soon",
+    avatar: AVATARS_TOM
+  }
+];
+
 const HeaderComponent = ({ showSearchModal }) => (
   <Header id="header">
     <div className="container-fluid">
@@ -64,10 +101,56 @@ const HeaderComponent = ({ showSearchModal }) => (
               <Icon type="search" />
             </Menu.Item>
 
-            <Menu.Item key="2">
-              <Badge count={5}>
-                <Icon type="bell" />
-              </Badge>
+            <Menu.Item key="2" className="dropdown">
+              <Dropdown
+                overlay={
+                  <Menu className="header-dropdown-tabs">
+                    <Tabs defaultActiveKey="1">
+                      <TabPane tab="Notifications (3)" key="1">
+                        <List
+                          itemLayout="horizontal"
+                          dataSource={notificationsData}
+                          style={{ padding: '0 20px' }}
+                          renderItem={item => (
+                            <List.Item>
+                              <List.Item.Meta
+                                avatar={<Avatar src={item.avatar} />}
+                                title={<a href="https://google.com">{item.title}</a>}
+                                description={item.desc}
+                              />
+                            </List.Item>
+                          )}
+                        />
+                      </TabPane>
+
+                      <TabPane tab="Messages (2)" key="2">
+                        <List
+                          itemLayout="horizontal"
+                          dataSource={messageData}
+                          style={{ padding: '0 20px' }}
+                          renderItem={item => (
+                            <List.Item>
+                              <List.Item.Meta
+                                avatar={<Avatar src={item.avatar} />}
+                                title={<a href="https://google.com">{item.title} <Icon type="check" style={{ color: 'grey', fontSize: 12, opacity: 0.5 }} /></a>}
+                                description={item.desc}
+                              />
+                            </List.Item>
+                          )}
+                        />
+                      </TabPane>
+                    </Tabs>
+                  </Menu>
+                }
+                trigger={["click"]}
+                placement="bottomRight"
+              >
+                <div>
+                  <Badge count={5}>
+                    <Icon type="bell" />
+                  </Badge>
+                </div>
+              </Dropdown>
             </Menu.Item>
             
             <Menu.Item key="3">
